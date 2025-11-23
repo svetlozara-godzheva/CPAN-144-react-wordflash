64c596ace70930e5690935f8857831afcae11bfe
svetlozara-godzheva <n01754024@humber.ca>
2025-11-23 15:49:16 -0500

    Hiding the header on the wordflash page

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,8 +1,34 @@
-import React from "react";
+"use client"
+import React, { useState } from "react";
+import { usePathname } from "next/navigation";
+
+
+const hiddenRoutes = ["/wordflash"];
 
 export default function Header() {
+    const [isVisible, setIsVisible] = useState(true);
+
+    const show = () => {
+        if (!isVisible) {
+            setIsVisible(true);
+        }
+    }
+
+    const hide = () => {
+        if (isVisible) {
+            setIsVisible(false);
+        }
+    }
+
+    if (hiddenRoutes.indexOf(usePathname()) >= 0) {
+        hide();
+    }
+    else {
+        show();
+    }
+
     return (
-        <div className="p-4 mb-4 bg-body-tertiary">
+        <div className={`p-4 mb-4 bg-body-tertiary ${!isVisible ? "hidden" : ""}`}>
             <div className="container-fluid">
                 <h1 className="display-5 fw-bold">Welcome to WordFlash</h1>
                 <p className="col fs-4">Flashcards are an effective and fast way of learning as it forces people to
